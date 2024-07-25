c28d8b3caac9dfd21b7e164b454b941103d33498
SantiagoDeStefano <142719621+SantiagoDeStefano@users.noreply.github.com>
2024-07-25 10:22:12 +0700

    Add files via upload

---
+++ cntGoodNodeBinTree.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+typedef long long ll;
+typedef double long db;
+typedef string str;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* inputTree() {
+   cout << "Enter the value for the root: ";
+   int rootValue;
+   cin >> rootValue;
+
+   TreeNode *root = new TreeNode(rootValue);
+   queue<TreeNode*> q;
+   q.push(root);
+
+   while(!q.empty()) {
+        TreeNode *current = q.front();
+        q.pop();
+        
+        cout << "Enter the left child of " << current->val << " (or -1 for null): ";
+        int leftValue;
+        cin >> leftValue;
+        
+        if(leftValue != -1) {
+            current->left = new TreeNode(leftValue);
+            q.push(current->left);
+        }
+
+        cout << "Enter the right child of " << current->val << " (or -1 for null): ";
+        int rightValue;
+        cin >> rightValue;
+
+        if(rightValue != -1) {
+            current->right = new TreeNode(rightValue);
+            q.push(current->right);
+        }
+    }
+    return root;
+}
+
+void printTree(TreeNode *root) {
+    if(root == nullptr) {
+        return;
+    }
+    
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while(!q.empty()) {
+        TreeNode *current = q.front();
+        q.pop();
+
+        cout << current->val << " ";
+
+        if(current->left != nullptr) {
+            q.push(current->left);
+        }
+
+        if(current->right != nullptr) {
+            q.push(current->right);
+        }
+    }
+    cout << endl;
+}
+
+void goodNodes_(TreeNode* root, int prevRoot_val, int& count) {
+    if(root == nullptr) {
+        return;
+    }
+    if(root->val >= prevRoot_val) {
+        count = count + 1;
+        prevRoot_val = root->val;
+    }
+    goodNodes_(root->left, prevRoot_val, count);
+    goodNodes_(root->right, prevRoot_val, count);
+}
+
+int goodNodes(TreeNode* root) {
+    int count = 0;
+    int rootVal = root->val;
+    goodNodes_(root, rootVal, count);
+    return count;
+}
+
+int main()
+{
+    TreeNode *root = inputTree();
+    cout << goodNodes(root);
+}   
