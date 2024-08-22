32781473bafd78bdee2a69a319649ef5b2846a49
Furry_Hunter <142719621+SantiagoDeStefano@users.noreply.github.com>
2024-08-22 22:38:54 +0700

    Add files via upload

---
+++ binTreePreoderTraversal.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+typedef long long ll;
+typedef double long db;
+typedef string str;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* inputTree() {
+   cout << "Enter the value for the root: ";
+   int rootValue;
+   cin >> rootValue;
+
+   TreeNode *root = new TreeNode(rootValue);
+   queue<TreeNode*> q;
+   q.push(root);
+
+   while(!q.empty()) {
+        TreeNode *current = q.front();
+        q.pop();
+        
+        cout << "Enter the left child of " << current->val << " (or -1 for null): ";
+        int leftValue;
+        cin >> leftValue;
+        
+        if(leftValue != -1) {
+            current->left = new TreeNode(leftValue);
+            q.push(current->left);
+        }
+
+        cout << "Enter the right child of " << current->val << " (or -1 for null): ";
+        int rightValue;
+        cin >> rightValue;
+
+        if(rightValue != -1) {
+            current->right = new TreeNode(rightValue);
+            q.push(current->right);
+        }
+    }
+    return root;
+}
+
+void printTree(TreeNode *root) {
+    if(root == nullptr) {
+        return;
+    }
+    
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while(!q.empty()) {
+        TreeNode *current = q.front();
+        q.pop();
+
+        cout << current->val << " ";
+
+        if(current->left != nullptr) {
+            q.push(current->left);
+        }
+
+        if(current->right != nullptr) {
+            q.push(current->right);
+        }
+    }
+    cout << endl;
+}
+
+void preorderTraversal_helper(TreeNode* root, vector<int>& result) {
+    if(root == nullptr) {
+        return;
+    }
+    result.push_back(root->val);
+    preorderTraversal_helper(root->left, result);
+    preorderTraversal_helper(root->right, result);
+}
+
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> result;
+    preorderTraversal_helper(root, result);
+    return result;        
+}
+
+int main()
+{
+    TreeNode *root = inputTree();
+    for(auto x : preorderTraversal(root)) {
+        cout << x << " ";
+    }
+}   
