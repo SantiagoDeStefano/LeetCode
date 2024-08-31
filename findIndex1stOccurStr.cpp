8f892113b6bba1e3fadb2091c081be69a382d1c7
Furry_Hunter <142719621+SantiagoDeStefano@users.noreply.github.com>
2024-08-31 22:10:59 +0700

    Update findIndex1stOccurStr.cpp

--- findIndex1stOccurStr.cpp
+++ findIndex1stOccurStr.cpp
@@ -7,6 +7,9 @@ typedef double long db;
 typedef string str;
 
 int strStr(string haystack, string needle) {
+    if (haystack.length() < needle.length()) {
+        return -1;
+    }
     for (int i = 0; i <= haystack.length() - needle.length(); i++) {
         if (haystack.substr(i, needle.length()) == needle) {
             return i;
